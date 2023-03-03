e62364e1e4df8d6507029191d41f55ce170b6784
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-03-03 22:33:13 +0100

    wersja2

---
+++ untitled/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 30;
+        char B = 'B';
+        System.out.println("Hello world!");
+        System.out.println("Kamila");
+        System.out.println(age);
+        System.out.println(B);
+
+    }
+}
