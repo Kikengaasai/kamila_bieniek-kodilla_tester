babacb065e20d4b5028bd9cfb8e57910b4d5daff
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-03-04 20:48:11 +0100

    Zadanie deklarowanie i wywoływanie zmiennych

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,21 @@
+public class MainExtended {
+    public static void main (String[] args){
+        String love = "Kocham Zebre";
+        int favouriteKamaNumber = 3;
+        char favouriteKamaLetter = 'K';
+        String country = "Zebry mieszkają w Afryce";
+        int favouriteZebraNumber = 5;
+        char favouriteZebraLetter = 'G';
+
+        System.out.println(love);
+        System.out.println(favouriteKamaNumber);
+        System.out.println(favouriteKamaLetter);
+        System.out.println(country);
+        System.out.println(favouriteZebraNumber);
+        System.out.println(favouriteZebraLetter);
+
+
+    }
+
+
+}
