8a3f0c9f777e115c64e045546f0eb06e0787a3e4
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-03-23 22:17:12 +0100

    FirstClass i Notebook

--- untitled/src/main/java/FirstClass.java
+++ untitled/src/main/java/FirstClass.java
@@ -1,7 +1,23 @@
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello World!"); // [3]
+public class FirstClass {
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook(600,1000, 1992);
+        System.out.println("notebook.weight:"+notebook.weight+" "+"notebook.price:"+notebook.price);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYearAndPrice();
+
+        Notebook heavyNotebook = new Notebook(2000,1500, 2001);
+        System.out.println("heavyNotebook.weight:"+heavyNotebook.weight+" "+"heavyNotebook.price:"+heavyNotebook.price);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYearAndPrice();
+
+        Notebook oldNotebook = new Notebook(1200,500, 2022);
+        System.out.println("oldNotebook.weight:"+oldNotebook.weight+" "+"oldNotebook.price:"+oldNotebook.price);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYearAndPrice();
+
+
     }
 }
-
-
---
+++ untitled/src/main/java/Notebook.java
@@ -0,0 +1,41 @@
+public class Notebook {
+    int weight;
+    int price;
+    int year;
+    public Notebook (int weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+
+    }
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if (this.price >= 600 && this.price <= 1000){
+            System.out.println("The price is good");
+        }else {
+            System.out.println("This notebook is expensive ");
+        }
+
+    }
+    public void checkWeight() {
+        if (this.weight < 500) {
+            System.out.println("This device is lightweight.");
+        } else if (this.weight >= 500 && this.weight <= 1000){
+            System.out.println("This device is not very heavy.");
+        }else {
+            System.out.println("This device is heavy.");
+        }
+
+    }
+
+    public void checkYearAndPrice(){
+        if(this.year <2000 && this.price > 1000){
+            System.out.println("Stary ale jary");
+        }else if ( this.year >= 2000 && this.price <2500){
+            System.out.println("Kupuj, ani się nie zastanawiaj");
+        }    else {
+            System.out.println("Zastanów się nad wyborem");
+        }
+    }
+}
