7eee913730c9ceacfe56ab4cf211755b9b6d3cac
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-04-03 21:01:07 +0200

    Grades

---
+++ src/main/java/Grades.java
@@ -0,0 +1,35 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int last() {
+        if (this.size == 0) {
+            return 0;
+        }
+        return grades[this.size - 1];
+    }
+
+    public double average() {
+        double sum = 0;
+        for (int i = 0; i < this.size; i++) {
+            sum = sum + grades[i];
+        }if (this.size==0){
+            return 0;
+        }
+        return sum / this.size;
+    }
+
+}
---
+++ src/main/java/GradesII.java
@@ -0,0 +1,10 @@
+public class GradesII {
+    public static void main(String[] args) {
+        Grades grades=new Grades();
+
+
+        System.out.println(grades.last());
+        System.out.println(grades.average());
+
+    }
+}
