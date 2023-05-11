cbc0c6c4e161e1e46a6564d8e765b07005c9b274
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-05-11 20:12:17 +0200

    Wersja5

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,12 +3,26 @@ public class Application {
     double age;
     double height;
 
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void validate() {
+        if (name != null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
-        Application Adam = new Application();
-
-        Adam.age=40.5;
-        Adam.height=178;
+        Application Adam = new Application("Adam", 40.5, 178);
+        Adam.validate();
 
 
     }
