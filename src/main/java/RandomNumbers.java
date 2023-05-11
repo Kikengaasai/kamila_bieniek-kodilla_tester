6a252a4ae93a541005a01f7fb6dea50ef79601eb
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-05-11 19:58:03 +0200

    Wersja4

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -5,19 +5,27 @@ public class RandomNumbers {
     public static void main(String[] args) {
 
         Random liczba = new Random();
-        int losowanie = liczba.nextInt(31);
+
 
         int sum = 0;
-        for (int i = 0; i < 1000; i++) {
+        int min = 30;
+        int max = 0;
+        while (sum < 5000) {
 
+            int losowanie = liczba.nextInt(31);
             sum = sum + losowanie;
 
-            if (sum >5000){
-                System.out.println("przekracza 5000");
-
-
+            if (losowanie < min) {
+                min = losowanie;
             }
+            if (losowanie > max) {
+                max = losowanie;
+            }
+
         }
+        System.out.println(min);
+        System.out.println(max);
+    }
 
 
-    }}
+}
