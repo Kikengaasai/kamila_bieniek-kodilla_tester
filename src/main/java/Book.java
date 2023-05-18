c942b223b2c964f6c170345c76affe244ba81276
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-05-18 17:49:21 +0200

    Wersja7

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,18 +1,21 @@
 public class Book {
-    private static String author;
+    private String author;
 
     String getAuthor() {
         return this.author;
     }
 
-    private static String title;
+    private String title;
 
     String getTitle() {
         return this.title;
     }
 
-    public static Book of(String author, String title) {
+    public static Book of() {
         Book book = new Book();
+        String author = book.getAuthor();
+        String title = book.getTitle();
+
         System.out.println("Author:"+ author);
         System.out.println("Title: "+ title);
         return book;
--- src/main/java/checkBook.java
+++ src/main/java/checkBook.java
@@ -1,7 +1,7 @@
 public class checkBook {
 
     public static void main(String[] args) {
-        Book book = Book.of("Jacek Piekara", "Ja, Inkwizytor");
+        Book book = Book.of();
 
     }
 }
