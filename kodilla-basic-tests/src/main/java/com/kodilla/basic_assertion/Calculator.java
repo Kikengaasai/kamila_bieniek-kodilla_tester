79c2cf7b8130f37bb8d690dfbe9f40cafd1a32e3
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-06-06 22:41:41 +0200

    Wersja11

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,20 @@
+
+package com.kodilla.basic_assertion;
+
+
+
+
+public class Calculator {
+
+    int sum ( int a, int b){
+        return a+b;
+    }
+
+    int subtract (int a, int b){
+        return a-b;
+    }
+
+    int squarePow (int a){
+        return a*a;
+    }
+}
