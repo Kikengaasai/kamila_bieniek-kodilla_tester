abfbb4875066c3def42b9c81191b42d4f0c2e3b8
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-06-13 17:44:45 +0200

    Wersja14

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Application.java
@@ -2,11 +2,23 @@ package com.kodilla.abstracts.homework;
 
 public class Application {
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
         Nurse nurse = new Nurse();
-Person person = new Person("Kamila", 30, nurse );
-System.out.println("Kamila's responsibilities include: " );
+        Person Kamila = new Person("Kamila", 30, nurse);
+        String kamilaResponsibilities = nurse.getResponsibilities();
+        System.out.println("Kamila's duties at work are: " + kamilaResponsibilities);
+
+        Manager manager = new Manager();
+        Person Marek = new Person("Marek", 37, manager);
+        String marekResponsibilities = manager.getResponsibilities();
+        System.out.println("Marek's duties at work are: "+marekResponsibilities);
+
+        Seller seller = new Seller();
+        Person Ola = new Person("Ola", 24, seller);
+        String olaResponsibilities = seller.getResponsibilities();
+        System.out.println("Ola's duties at work are: "+olaResponsibilities);
+
 
     }
 }
