117f7eee74799e61b1c547a900a89b3a039a3cce
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-06-15 19:11:18 +0200

    Wersja22

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarApplication.java
@@ -0,0 +1,41 @@
+package com.kodilla.collections.arrays.homework;
+
+
+import com.kodilla.collections.arrays.ShapeUtils;
+import com.kodilla.collections.interfaces.homework.Audi;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Honda;
+import com.kodilla.collections.interfaces.homework.Opel;
+
+import java.util.Random;
+
+public class CarApplication {
+
+    public static void main(String[] args) {
+
+        Car[] cars = new Car[15];
+        for(int n=0; n< cars.length; n++)
+            cars[n] = drawCar();
+        for (Car car : cars)
+            CarUtils.describeCar(car);
+    }
+
+    public static Car drawCar() {
+        Random random = new Random();
+        int drawnCarKind = random.nextInt(3);
+        int a = getRandomIncreaseSpeed(random);
+        if (drawnCarKind == 0)
+            return new Honda(a);
+        else if (drawnCarKind == 1)
+            return new Opel(a);
+        else {
+            return new Audi(a);
+
+
+        }
+    }
+
+    private static int getRandomIncreaseSpeed(Random random) {
+        return random.nextInt() + 10;
+    }
+}
---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -0,0 +1,27 @@
+package com.kodilla.collections.arrays.homework;
+
+import com.kodilla.collections.interfaces.homework.Audi;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Honda;
+import com.kodilla.collections.interfaces.homework.Opel;
+
+public class CarUtils {
+
+    public static void describeCar(Car car){
+        System.out.println("-----------------------------");
+        System.out.println("Car kind: " + getCarName(car));
+        System.out.println("Car speed: " + car.getSpeed());
+
+
+    }
+    private static String getCarName(Car car) {
+        if (car instanceof Honda)
+            return "Honda";
+        else if (car instanceof Opel)
+            return "Opel";
+        else if (car instanceof Audi)
+            return "Audi";
+        else
+            return "Unknown car name";
+    }
+}
