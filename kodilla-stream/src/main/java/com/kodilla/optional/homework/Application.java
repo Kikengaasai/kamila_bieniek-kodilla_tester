f51d3fe5e6fba5d8f525ed85795fce080cb76774
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-06-18 00:03:42 +0200

    Wersja31

---
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -0,0 +1,28 @@
+package com.kodilla.optional.homework;
+
+import com.kodilla.stream.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Application {
+    public static void main(String[] args) {
+
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student ("Karol", null));
+        students.add(new Student ("Andrzej", new Teacher("Konstanty")));
+        students.add(new Student ("Jola", new Teacher("Gregoria")));
+        students.add(new Student ("Mordimer", new Teacher("Aldona")));
+        students.add(new Student ("Joanna", new Teacher("Kunegunda")));
+        students.add(new Student ("Grzegorz", null));
+
+        for (Student student : students )
+            System.out.println("Uczeń: " + student.getName() + "   Nauczyciel: " + student.getTeacher());
+
+
+        Optional<Student> optionalStudent = Optional.ofNullable(new Student("Kasia",new Teacher("<undefined>")));
+        optionalStudent.ifPresent(u -> System.out.println("<undefined>"));
+    }
+}
