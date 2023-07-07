c0e10b3a0a6419c6c3b99b3fd50455281010f9a1
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-07-07 23:18:49 +0200

    Wersja45

--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherNotificationServiceTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherNotificationServiceTestSuite.java
@@ -1,9 +1,11 @@
 package com.kodilla.mockito.homework;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
 
 class WeatherNotificationServiceTestSuite {
     private WeatherNotificationService weatherNotificationService = Mockito.mock(WeatherNotificationService.class);
@@ -12,20 +14,46 @@ class WeatherNotificationServiceTestSuite {
 
     @Test
     public void shouldSubscribeUserToGivenLocation() {//SUBSKRYBCJA PODANEJ LOKALIZACJI
+        String message = "You subscribe to this location: " + location;
         weatherNotificationService.subscribe(email, location);
+        weatherNotificationService.sendSubscribersLocation(location,message);
         Mockito.verify(weatherNotificationService).subscribe(email, location);
+       Mockito.verify(weatherNotificationService).sendSubscribersLocation(location,message);
+
+
     }
 
     @Test
-    public void shouldUnsubscribeUserFromAllLocations() {//POWINNO USUNĄĆ SUBSKRYPCJE Z WSZYSTKICH LOKALIZACJI
+    public void shouldUnsubscribeUserFromAllLocations() {
+
+        ArgumentCaptor<String> messageCaptor = ArgumentCaptor.forClass(String.class);
+        String expectedMessage = "Your subscription has been deactivated from all locations";
+
+
         weatherNotificationService.unsubscribe(email);
+        weatherNotificationService.sendAll(expectedMessage);
+
+
         Mockito.verify(weatherNotificationService).unsubscribe(email);
+        Mockito.verify(weatherNotificationService).sendAll(messageCaptor.capture());
+        assertEquals(expectedMessage, messageCaptor.getValue());
     }
 
+
     @Test
-    public void shouldUnsubscribeUserFromSpecificLocation() {// POWINNO USUNĄĆ SUBSKRYPCJE PODANEJ LOKALIZACJI
+    public void shouldUnsubscribeUserFromSpecificLocation() {
+        // given
+        ArgumentCaptor<String> messageCaptor = ArgumentCaptor.forClass(String.class);
+        String expectedMessage = "Your subscription has been disabled at the location:" + location;
+
+        // when
         weatherNotificationService.unsubscribe(email, location);
+        weatherNotificationService.sendSubscribersLocation(expectedMessage, location);
+
+        // then
         Mockito.verify(weatherNotificationService).unsubscribe(email, location);
+        Mockito.verify(weatherNotificationService).sendSubscribersLocation(messageCaptor.capture(), eq(location));
+        assertEquals(expectedMessage, messageCaptor.getValue());
     }
 
     @Test
