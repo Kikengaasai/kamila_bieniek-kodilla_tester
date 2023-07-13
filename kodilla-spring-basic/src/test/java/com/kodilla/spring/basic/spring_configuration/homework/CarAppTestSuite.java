360ee1ecfad5d3d6099c12826ab7429e8e4c3515
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-07-13 21:52:07 +0200

    Wersja51

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarAppTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarAppTestSuite.java
@@ -1,29 +1,29 @@
 package com.kodilla.spring.basic.spring_configuration.homework;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CarAppTestSuite {
+
+    @Autowired
+    private Car car;
 
     @Test
     public void shouldTurnOnHeadlightsWhenIsNight() {
-        CarApp carApp = new CarApp();
-        Car car = carApp.car();
         assertTrue(car.hasHeadlightsTurnedOn());
     }
 
     @Test
     public void shouldReturnSedanForSpring() {
-        CarApp carApp = new CarApp();
-        Car car = carApp.car();
         if(car.getMonth()>= 3 && car.getMonth() <= 5){
         assertEquals("Sedan", car.getCarType());}
     }
     @Test
     public void shouldReturnSedanForAutumn() {
-        CarApp carApp = new CarApp();
-        Car car = carApp.car();
         if (car.getMonth() >= 9 && car.getMonth() <= 11) {
             assertEquals("Sedan", car.getCarType());
         }
@@ -31,16 +31,12 @@ class CarAppTestSuite {
 
     @Test
     public void shouldReturnCabrioForSummer() {
-        CarApp carApp = new CarApp();
-        Car car = carApp.car();
         if (car.getMonth() >= 6 && car.getMonth() <= 8) {
             assertEquals("Cabrio", car.getCarType());
         }
     }
     @Test
     public void shouldReturnSUVForWinter() {
-        CarApp carApp = new CarApp();
-        Car car = carApp.car();
         if (car.getMonth() >= 1 && car.getMonth() <= 2 || car.getMonth() == 12) {
             assertEquals("SUV", car.getCarType());
         }
