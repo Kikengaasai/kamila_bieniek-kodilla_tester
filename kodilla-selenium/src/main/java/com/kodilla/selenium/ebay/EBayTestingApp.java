96b9a5410db08c1d51c7f0d837d1d84b2d5c27fe
Kamila Bieniek <bieniek.kamila@gmail.com>
2023-07-27 20:36:22 +0200

    Wersja57

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -1,19 +1,18 @@
-package com.kodilla.selenium.search;
+package com.kodilla.selenium.ebay;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class StoreSearchExample {
+public class EBayTestingApp {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kamil\\Selenium-drivers\\Chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://kodilla.com/pl/test/store");
+        driver.get("https://ebay.com");
 
-        WebElement inputField = driver.findElement(By.name("search"));
-        inputField.sendKeys("School");
+        WebElement inputField = driver.findElement(By.name("_nkw"));
+        inputField.sendKeys("Laptop");
         inputField.submit();
-
     }
 }
